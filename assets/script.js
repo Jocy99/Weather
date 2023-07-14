5940948e906cadee52f12ee1a0c61cc4afc0e037
Joce <jocelynn4355@gmail.com>
2023-07-13 22:53:04 -0500

    added js code

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,36 @@
 var apiKey = 'ec0abd7563a7622e43e501bd5d7eefbc';
-var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?units=metrics&q=';
+var forecastApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=';
+var currentWeatherApiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q=';
+
 // is this url workinh? or is it my api key?
-var choosenCity = document.querySelector('citySearch');
+var choosenCity = document.querySelector('#citySearch');
 var searchBtn = document.querySelector('.searchBtn');
 
-function checkWeather (choosenCity) {
-    var response = fetch(apiUrl + '&appid=${apiKey}');
-    var weatherData = response.json();
+// 
+async function checkWeather(cityField) {
+    // Request 1: Current Weather
+    var currentWeatherResponse =  await fetch(currentWeatherApiUrl + cityField + '&appid=' + apiKey);
+    var currentWeatherData = await currentWeatherResponse.json();
+    console.log("CURRENT WEATHER", currentWeatherData);
 
-    console.log(weatherData);
+    // Request 2: 5 day forecast
+    var forecastResponse =  await fetch(forecastApiUrl + cityField + '&appid=' + apiKey);
+    var forecastWeatherData = await forecastResponse.json();
+    console.log("FORECAST DATA", forecastWeatherData);
 
-    document.querySelector('.city').innerHTML.html = data.name;
-    document.querySelector('.date').innerHTML.html = data.dt_txt;
-    document.querySelector('.current-temp').innerHTML.html = Math.round(data.main.temp) + '℃';
-    document.querySelector('.current-wind').innerHTML.html = data.wind.speed + 'km/h';
-    document.querySelector('.current-humidity').innerHTML.html = data.name.humidity + '%';
-console.log('HI!!!')
+    // Get 5 days out of the list
+    console.log(forecastWeatherData.list[0]);
+    var data = forecastWeatherData.list[0];
+    document.querySelector('.city').innerHTML = data.city.name;
+    document.querySelector('.date').innerHTML = data.dt_txt;
+    document.querySelector('.current-temp').innerHTML = Math.round(data.main.temp) + 'F';
+    document.querySelector('.current-wind').innerHTML = data.wind.speed + 'km/h';
+    document.querySelector('.current-humidity').innerHTML = data.name.humidity + '%';
+    // console.log('HI!!!')
 }
 
 searchBtn.addEventListener('click', ()=>{
-    checkWeather(citySearch.value);
-})
-checkWeather(city);
+    checkWeather(choosenCity.value);
+});
+
+checkWeather(choosenCity.value);
